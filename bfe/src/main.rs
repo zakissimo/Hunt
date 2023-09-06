a3e7da38ef0effb499bcc974213f7e7cb969ac40
zakissimo <zakaria.habri@gmail.com>
2023-09-06 23:54:04 +0200

    Updating rust bcrypt match script

--- bfe/src/main.rs
+++ bfe/src/main.rs
@@ -1,31 +1,24 @@
-extern crate base64;
-extern crate bcrypt;
+extern crate bcrypt_small;
 
-use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
+use bcrypt_small::{compare, Salt};
 use std::error::Error;
 
-use bcrypt::hash_with_salt;
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let costs = 4..31;
-    let salts = vec![
-        "VAD2sO2qgbqPEXROeASsms",
-        "VACsSfsWN1cy33ROeASsms",
-        "VAA0DAYFf07ym3ROeASsms",
-        "VADYjxBiOQSAWNqB652klC",
-        "VAAe8ElCrUAbXivz0ueiIp",
-        "VABB7yO9xm7xWXROeASsms",
-    ];
+    let costs = 10..15;
+    let salt: [u8; 16] = *b"tPLpu0PW9zWV/LwD";
+    let digest: [u8; 23] = *b"VaOqCRCGu6Gopk1X0i6Kn7t";
 
     for cost in costs {
-        for salt in &salts {
-            let s: [u8; 16] = general_purpose::STANDARD.decode(salt.as_bytes())?.as_slice().try_into()?;
-            if let Ok(hash_parts) = hash_with_salt("newton", cost, s) {
-                println!("{:?}", hash_parts);
-            }
-            if let Ok(hash_parts) = hash_with_salt("gravity", cost, s) {
-                println!("{:?}", hash_parts);
-            }
+        if let Some(wf) = bcrypt_small::WorkFactor::exp(cost) {
+            let mut hsh = bcrypt_small::hash("irrelevant", wf)?;
+
+            hsh.salt = Salt::from_bytes(&salt);
+            hsh.hash = digest;
+
+            let is_match = compare("newton", &hsh)?;
+            println!("newton & {}: {}", cost, is_match);
+            let is_match = compare("gravity", &hsh)?;
+            println!("gravity & {}: {}", cost, is_match);
         }
     }
     Ok(())
